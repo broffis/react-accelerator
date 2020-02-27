e5257575cfc3354d68a24c9bbf2b12b003cb0c48
Brandon Roffis <broffis@outlook.com>
2020-02-27 16:31:55 -0500

    Ejecting for modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`
 
 class App extends Component {
   state = {
@@ -47,11 +62,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      }
     };
 
     let persons = null;
@@ -63,15 +83,31 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
-       <h1>Hi, I'm a react app</h1>
-       <button
-        style={style}
-        onClick={this.togglePersonHandler}>Toggle Persons</button>
-        { persons }
+        <h1>Hi, I'm a react app</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button className="button" alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </button>
+          { persons }
       </div>
     );
   }
