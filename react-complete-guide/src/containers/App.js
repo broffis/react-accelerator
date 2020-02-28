8215602bd52e1db258b9189e082e36b3e9dd2235
Brandon Roffis <broffis@outlook.com>
2020-02-28 12:32:24 -0500

    Refactored everything into sub components

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import classes from  './App.css';
 
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -47,36 +48,23 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-           return <Person key={person.id} name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-        </div>
+        <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}/>
       );
-
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a react app</h1>
-        <p className={assignedClasses.join(' ')}>This is really working</p>
-        <button className={btnClass} onClick={this.togglePersonHandler}>
-          Toggle Persons
-        </button>
-          { persons }
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonHandler} />
+        { persons }
       </div>
     );
   }
